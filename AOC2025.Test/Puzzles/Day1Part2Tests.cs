b8c20bbd37fe811b0ad94ab28d327d5ac80b8d88
Phillip Hunter <phunter95@gmail.com>
2025-12-05 00:59:29 -0500

    Day 1 Part 2 WIP

---
+++ AOC2025.Test/Puzzles/Day1Part2Tests.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using AOC2025.Puzzles;
+
+namespace AOC2025.Tests.Puzzles
+{
+    public class Day1Part2Tests
+    {
+        [Fact]
+        public void NameField_WhenCalled_ReturnsProperFormat()
+        {
+            CommonTests.ValidateNameField<Day1Part2>();
+        }
+
+        [Fact]
+        public void GetOutput_WhenCalledWithExample_ReturnsExampleAnswer()
+        {
+            CommonTests.ValidateOutput<Day1Part2>(true);
+        }
+
+        [Fact]
+        public void GetOutput_WhenCalledWithoutExample_ReturnsAnswer()
+        {
+            CommonTests.ValidateOutput<Day1Part2>(false);
+        }
+
+        [Theory]
+        [InlineData(50, -68, 82, 1)]
+        [InlineData(52, 48, 0, 1)]
+        [InlineData(20, -20, 0, 1)]
+        [InlineData(20, 80, 0, 1)]
+        [InlineData(88, 479, 67, 5)]
+        [InlineData(57, -238, -81, 2)]
+        [InlineData(0, -5, 95, 0)]
+        public void CalculateNewPosition_WhenCalled_ReturnsProperResult(
+            int currentPos,
+            int movement,
+            int expected,
+            int? expectedClicksAtZero
+        )
+        {
+            // Act
+            var result = Day1Part2.CalculateNewPosition(
+                currentPos,
+                movement,
+                out var resultClicksAtZero
+            );
+
+            // Assert
+            Assert.Equal(expected, result);
+
+            if (expectedClicksAtZero != null)
+                Assert.Equal(expectedClicksAtZero, resultClicksAtZero);
+        }
+
+        [Theory]
+        [InlineData("R20", 20)]
+        [InlineData("L35", -35)]
+        public void RotationTextToMovement_WhenCalled_ReturnsProperResult(
+            string rotationText,
+            int expected
+        )
+        {
+            // Act
+            var result = Day1Part2.RotationTextToMovement(rotationText);
+
+            // Assert
+            Assert.Equal(expected, result);
+        }
+    }
+}
---
+++ AOC2025/Puzzles/Day1Part2.cs
@@ -0,0 +1,107 @@
+﻿using System.Configuration;
+using System.Diagnostics;
+
+namespace AOC2025.Puzzles
+{
+    public class Day1Part2 : IAdventPuzzle
+    {
+        public string Name => "Day 1: Secret Entrance Part 2";
+        public string? Solution => "-1337";
+        public string? ExampleSolution => "6";
+        public bool ExampleRun { get; set; } = false;
+
+        private string _filename = "Day1.txt";
+
+        public const int StartingPosition = 50;
+        public const int TotalPositions = 100;
+
+        public PuzzleOutput GetOutput()
+        {
+            var stopwatch = Stopwatch.StartNew();
+
+            #region Puzzle
+
+            if (ExampleRun)
+            {
+                _filename =
+                    $"{Path.GetFileNameWithoutExtension(_filename)}Ex{Path.GetExtension(_filename)}";
+            }
+
+            var path = ConfigurationManager.AppSettings["PuzzleInputDirectory"];
+
+            var currPuzzleFileLines = File.ReadAllLines(
+                Path.Combine(
+                    ConfigurationManager.AppSettings["PuzzleInputDirectory"]
+                        ?? "../../../../Inputs/",
+                    _filename
+                )
+            );
+
+            var currentPosition = StartingPosition;
+            var result = 0;
+
+            foreach (var currPuzzleFileLine in currPuzzleFileLines)
+            {
+                var currentMovement = RotationTextToMovement(currPuzzleFileLine);
+                currentPosition = CalculateNewPosition(
+                    currentPosition,
+                    currentMovement,
+                    out var clicksAtZero
+                );
+
+                //if (currentPosition == 0)
+                //{
+                //    result++;
+                //}
+
+                result += clicksAtZero;
+
+                AOC.Log(
+                    $"{currPuzzleFileLine} land at {currentPosition} with {clicksAtZero} clicks at zero. Total zeros: {result}"
+                );
+            }
+
+            #endregion Puzzle
+
+            stopwatch.Stop();
+
+            var puzzleOutput = new PuzzleOutput
+            {
+                Result = result.ToString(),
+                CompletionTime = stopwatch.ElapsedMilliseconds,
+            };
+
+            return puzzleOutput;
+        }
+
+        public static int RotationTextToMovement(string rotationText)
+        {
+            var intPart = int.Parse(rotationText.Substring(1));
+            var sign = (rotationText.FirstOrDefault() == 'L') ? -1 : 1;
+
+            return intPart * sign;
+        }
+
+        public static int CalculateNewPosition(int currentPos, int movement, out int clicksAtZero)
+        {
+            var sum = (currentPos + movement);
+            var landPos = (TotalPositions + sum) % TotalPositions;
+
+            if (sum == 0)
+            {
+                clicksAtZero = 1;
+            }
+            else
+            {
+                clicksAtZero = Math.Abs((sum - (sum % 100)) / TotalPositions);
+            }
+
+            if (currentPos != 0 && sum < 0)
+            {
+                clicksAtZero++;
+            }
+
+            return landPos;
+        }
+    }
+}
